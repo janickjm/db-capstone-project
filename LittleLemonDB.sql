3efd3cf9d09411e2d1667068f0857901990e098f
janickjm <janickjeanmarie@gmail.com>
2025-10-29 16:18:14 -0400

    ajout des fichiers dans la branche command

---
+++ LittleLemonDB.sql
@@ -0,0 +1,174 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema LittleLemonDB
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema LittleLemonDB
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `LittleLemonDB` DEFAULT CHARACTER SET utf8 ;
+USE `LittleLemonDB` ;
+
+-- -----------------------------------------------------
+-- Table `LittleLemonDB`.`Clients`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Clients` (
+  `ClientID` INT NOT NULL,
+  `FirstName` VARCHAR(80) NOT NULL,
+  `LastName` VARCHAR(45) NOT NULL,
+  `Phone` VARCHAR(14) NOT NULL,
+  PRIMARY KEY (`ClientID`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `LittleLemonDB`.`Roles`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Roles` (
+  `RoleID` INT NOT NULL,
+  `Name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`RoleID`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `LittleLemonDB`.`Staff`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Staff` (
+  `StaffID` INT NOT NULL,
+  `FirstName` VARCHAR(80) NOT NULL,
+  `LastName` VARCHAR(80) NOT NULL,
+  `Salary` DECIMAL(8,2) NOT NULL,
+  `RoleID` INT NOT NULL,
+  PRIMARY KEY (`StaffID`),
+  INDEX `fk_staff_role_id_idx` (`RoleID` ASC) VISIBLE,
+  CONSTRAINT `fk_staff_role_id`
+    FOREIGN KEY (`RoleID`)
+    REFERENCES `LittleLemonDB`.`Roles` (`RoleID`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `LittleLemonDB`.`Booking`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Booking` (
+  `BookingID` INT NOT NULL,
+  `BookingSlot` DATETIME NOT NULL,
+  `Table` INT NOT NULL,
+  `ClientID` INT NOT NULL,
+  `StaffID` INT NOT NULL,
+  PRIMARY KEY (`BookingID`),
+  INDEX `client_id_fk_idx` (`ClientID` ASC) VISIBLE,
+  INDEX `fk_booking_StaffID_idx` (`StaffID` ASC) VISIBLE,
+  CONSTRAINT `client_id_fk`
+    FOREIGN KEY (`ClientID`)
+    REFERENCES `LittleLemonDB`.`Clients` (`ClientID`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_booking_StaffID`
+    FOREIGN KEY (`StaffID`)
+    REFERENCES `LittleLemonDB`.`Staff` (`StaffID`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `LittleLemonDB`.`Menu`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Menu` (
+  `MenuID` INT NOT NULL,
+  `Cuisine` VARCHAR(45) NOT NULL,
+  `Name` VARCHAR(45) NOT NULL,
+  `Type` VARCHAR(45) NOT NULL,
+  `Price` DECIMAL(5,2) NOT NULL,
+  `Available` TINYINT NOT NULL,
+  PRIMARY KEY (`MenuID`),
+  UNIQUE INDEX `MenuID_UNIQUE` (`MenuID` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `LittleLemonDB`.`Orders`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Orders` (
+  `OrderID` INT NOT NULL,
+  `Date` DATETIME NOT NULL,
+  `Quantity` INT NOT NULL,
+  `Total` DECIMAL(8,2) NOT NULL,
+  `MenuID` INT NOT NULL,
+  `StaffID` INT NOT NULL,
+  `BookingID` INT NOT NULL,
+  PRIMARY KEY (`OrderID`),
+  INDEX `menu_id_fk_idx` (`MenuID` ASC) VISIBLE,
+  INDEX `booking_id_fk_idx` (`BookingID` ASC) VISIBLE,
+  INDEX `fk_Orders_StaffID_idx` (`StaffID` ASC) VISIBLE,
+  CONSTRAINT `menu_id_fk`
+    FOREIGN KEY (`MenuID`)
+    REFERENCES `LittleLemonDB`.`Menu` (`MenuID`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `booking_id_fk`
+    FOREIGN KEY (`BookingID`)
+    REFERENCES `LittleLemonDB`.`Booking` (`BookingID`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_Orders_StaffID`
+    FOREIGN KEY (`StaffID`)
+    REFERENCES `LittleLemonDB`.`Staff` (`StaffID`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `LittleLemonDB`.`Status`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Status` (
+  `StatusID` INT NOT NULL,
+  `Status` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`StatusID`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `LittleLemonDB`.`OrderDelivery`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`OrderDelivery` (
+  `OrderDeliveryID` INT NOT NULL,
+  `Date` DATETIME NOT NULL,
+  `OrderID` INT NOT NULL,
+  `StatusID` INT NOT NULL,
+  `StaffID` INT NOT NULL,
+  PRIMARY KEY (`OrderDeliveryID`),
+  INDEX `status_id_fk_idx` (`StatusID` ASC) VISIBLE,
+  INDEX `order_id_fk_idx` (`OrderID` ASC) VISIBLE,
+  INDEX `fk_OrderDelivery_StaffID_idx` (`StaffID` ASC) VISIBLE,
+  CONSTRAINT `status_id_fk`
+    FOREIGN KEY (`StatusID`)
+    REFERENCES `LittleLemonDB`.`Status` (`StatusID`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `order_id_fk`
+    FOREIGN KEY (`OrderID`)
+    REFERENCES `LittleLemonDB`.`Orders` (`OrderID`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_OrderDelivery_StaffID`
+    FOREIGN KEY (`StaffID`)
+    REFERENCES `LittleLemonDB`.`Staff` (`StaffID`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
